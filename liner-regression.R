97a78d7c46a642939722b50183d2864f90640116
joseph-akintunde <57961001+joseph-akintunde@users.noreply.github.com>
2025-09-04 14:38:23 -0400

    Add files via upload

--- liner-regression.R
+++ liner-regression.R
@@ -3,3 +3,6 @@ scatter.smooth(x=cars$mpg, y=cars$disp, main="SpeedVSdistance")
 cor(cars$mpg, cars$disp)]
 training_data <- cars[1:16,]
 test_data <- cars[17:32,]
+regression_result <- lm(mpg ~ disp, data=training_data)
+prediction_result <- predict(regression_result, test_data)
+actual_prediction_values <- data.frame(cbind(actuals=training_data$mpg, predicteds=prediction_result))
